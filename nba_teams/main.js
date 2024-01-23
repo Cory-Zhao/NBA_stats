1b5c0065b5b56014b89ab85b6c1dc168b737e065
Cory Zhao <coryzhao@ucsb.edu>
2024-01-22 22:00:59 -0800

    Finished game stats implementation

--- nba_teams/main.js
+++ nba_teams/main.js
@@ -2,7 +2,7 @@
 const { connection } = require('./dbUtils');
 const { createNbaTeamsDB } = require('./create_teams_db');
 const { createTableTeams, fetchDataAndInsertIntoTeams } = require('./table_teams');
-
+const { createTableGameStats, fetchDataAndInsertIntoGameStats } = require('./table_game_stats');
 // Use async/await to ensure the order of execution
 (async () => {
     connection.connect();
@@ -11,6 +11,8 @@ const { createTableTeams, fetchDataAndInsertIntoTeams } = require('./table_teams
         await createNbaTeamsDB();
         await createTableTeams();
         await fetchDataAndInsertIntoTeams();
+        await createTableGameStats();
+        await fetchDataAndInsertIntoGameStats();
     } catch (error) {
         console.error('Error:', error);
     } finally {
---
+++ nba_teams/table_game_stats.js
@@ -0,0 +1,73 @@
+const fetch = require('node-fetch');
+const { connection, queryAsync } = require('./dbUtils');
+
+const createTableGameStats = async () => {
+    try {
+        const queryUse = 'USE nba_teams';
+        const queryTableGameStats = `CREATE TABLE IF NOT EXISTS game_stats (
+            game_id int NOT NULL, 
+            home_team_id tinyint(2) NOT NULL, 
+            visitor_team_id tinyint(2) NOT NULL, 
+            home_team_score tinyint(3) NOT NULL, 
+            visitor_team_score tinyint(3) NOT NULL, 
+            season int NOT NULL, 
+            postseason boolean NOT NULL, 
+            PRIMARY KEY (game_id)
+        );`;
+
+        await queryAsync(queryUse);
+        console.log('Database used!');
+
+        await queryAsync(queryTableGameStats);
+        console.log('Table created!');
+    } catch (error) {
+        console.error('Error creating table:', error);
+    } 
+};
+
+const fetchDataAndInsertIntoGameStats = async () => {
+    const date = new Date();
+    const parameters = {
+        season: 2023,
+        start_date: '2023-10-24',
+        end_date: date.getFullYear().toString() + '-' + (date.getMonth() + 1).toString().padStart(2,'0') + '-' + date.getDate().toString().padStart(2,'0'),
+        per_page: 100,
+    };
+    let page = 1;
+    
+    try {
+        const apiUrl = `https://balldontlie.io/api/v1/games?seasons[]=${parameters.season}&start_date=${parameters.start_date}&end_date=${parameters.end_date}&per_page=${parameters.per_page}&page=${page}`;
+        const response = await fetch(apiUrl);
+        if (!response.ok) {
+            if (response.status === 404) {
+                throw new Error('Data not found');
+            } else if (response.status === 500) {
+                throw new Error('Server error');
+            } else {
+                throw new Error('Network response was not ok');
+            }
+        }
+        const data = await response.json();
+
+        connection.beginTransaction();
+
+        const queryInsert = `INSERT IGNORE INTO game_stats (game_id, home_team_id, visitor_team_id, home_team_score, visitor_team_score, season, postseason) VALUES ?`;
+    
+        for (const game of data.data) {
+            const values = [[game.id, game.home_team.id, game.visitor_team.id, game.home_team_score, game.visitor_team_score, game.season, game.postseason]];
+            await queryAsync(queryInsert, [values]);
+            console.log(`Data inserted successfully for game { ID: ${game.id}, ${game.home_team.abbreviation} vs ${game.visitor_team.abbreviation} }`);
+        }
+        connection.commit();
+    }
+    catch (error) {
+        connection.rollback();
+        console.error('Error:', error);
+    }
+
+};
+
+module.exports = {
+    createTableGameStats,
+    fetchDataAndInsertIntoGameStats,
+}
